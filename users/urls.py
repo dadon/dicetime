62459f2cbd963dd2b2fc1b16f71400bd7195af40
vsemenkovv <v.semenkov.v@gmail.com>
2020-04-02 23:02:44 +0300

    for dev in users=urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from dice_time.settings import API_TOKEN, ORIGIN
-from . import views
-from .bot import bot
+#from . import views
+#from .bot import bot
 
 #bot.set_webhook(ORIGIN + 'tg/' + API_TOKEN)
 urlpatterns = [
-    path('tg/' + API_TOKEN, views.tg_webhook),
+    #path('tg/' + API_TOKEN, views.tg_webhook),
 
 ]

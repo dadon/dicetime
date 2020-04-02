b8df2dbc0179ba63568e40b5cc74099f0a7494a2
vsemenkovv <v.semenkov.v@gmail.com>
2020-04-02 23:04:10 +0300

    for dev in users urls+views.py

--- users/views.py
+++ users/views.py
@@ -7,10 +7,10 @@ from .models import *
 from django.shortcuts import HttpResponse
 
 
-bot.polling(none_stop=True, interval=0)
+#bot.polling(none_stop=True, interval=0)
 # Telegram Webhook handler
 @csrf_exempt
 def tg_webhook(request):
-    #bot.process_new_updates([ types.Update.de_json(request.body.decode("utf-8")) ])
-    #return HttpResponse('OK')
-    pass
+    bot.process_new_updates([ types.Update.de_json(request.body.decode("utf-8")) ])
+    return HttpResponse('OK')
+    #pass

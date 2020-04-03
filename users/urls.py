7d91619acfc4582e7fbc199bf6abb88933ced716
Ivan Kotelnikov <ivan.kotelnikov@bitfury.com>
2020-04-04 01:58:54 +0300

    comment dev lines

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from dice_time.settings import API_TOKEN, ORIGIN
-from . import views
-# from .bot import bot
+#from . import views
+#from .bot import bot
 
 #bot.set_webhook(ORIGIN + 'tg/' + API_TOKEN)
 urlpatterns = [

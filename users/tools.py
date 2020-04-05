0eddcb0b3cc04e7d4dd2dd79ac76f6f5f7fcdc92
Ivan Kotelnikov <ivan.kotelnikov@bitfury.com>
2020-04-05 19:48:44 +0300

    fix rotating handler import

--- users/tools.py
+++ users/tools.py
@@ -1,6 +1,7 @@
 import logging
 import sys
 
+from logging.handlers import TimedRotatingFileHandler
 from dice_time.settings import LOCAL
 
 logger = logging.getLogger('Dice')
@@ -10,7 +11,7 @@ def log_setup(loglevel=logging.INFO):
     formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     handlers = [logging.StreamHandler(sys.stdout)]
     if not LOCAL:
-        handlers.append(logging.TimedRotatingFileHandler(
+        handlers.append(TimedRotatingFileHandler(
             "debug.log", when='midnight', utc=True, backupCount=7))
 
     for h in handlers:

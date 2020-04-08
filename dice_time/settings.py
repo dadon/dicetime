210c09be3d2caffcfd9c628218578023b7460917
Ivan Kotelnikov <ivan.kotelnikov@bitfury.com>
2020-04-08 14:30:53 +0300

    static

--- dice_time/settings.py
+++ dice_time/settings.py
@@ -138,7 +138,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/var/www/static'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'pictures')
 MEDIA_URL = '/pictures/'
 

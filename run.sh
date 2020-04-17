557db0201872764c9b08a5607e40ad6102d5e970
Ivan Kotelnikov <ivan.kotelnikov@bitfury.com>
2020-04-18 02:55:36 +0300

    increase queue size

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 ./manage.py webhook
-screen -S celery -dm celery worker -A celery_app --concurrency=8
-screen -S dice -dm gunicorn --access-logfile gunicorn.log --workers 4 --bind 127.0.0.1:8000 --pid gunicorn.pid dice_time.wsgi:application
+screen -S celery -dm celery worker -A celery_app --concurrency=4
+screen -S dice -dm gunicorn --access-logfile gunicorn.log --workers 1 --bind 127.0.0.1:8000 --pid gunicorn.pid dice_time.wsgi:application
 screen -S jobs -dm ./manage.py startjobs
 screen -ls

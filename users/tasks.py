6618b02c4291b00cf91db13e1fd86a81809f7fa6
Ivan Kotelnikov <ivan.kotelnikov@bitfury.com>
2020-04-18 02:57:48 +0300

    ~

--- users/tasks.py
+++ users/tasks.py
@@ -12,12 +12,13 @@ logger = logging.getLogger('Dice')
 @app.task
 def tg_webhook_task(payload, pending_updates_skip_until):
     try:
-        logger.debug(f'Update: {payload}')
         update = types.Update.de_json(payload)
         if update.message and update.message.date <= pending_updates_skip_until:
             logger.info(f'#### Skipping this update')
             return
+        logger.debug(f'Update: {payload}')
         bot.process_new_updates([update])
+        logger.debug('-----------------')
     except Exception as exc:
         logger.error(f'{type(exc)}: {exc}')
         logger.error(traceback.format_exc())

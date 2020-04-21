f8fe7487d4a8075fe842590ed38b4d3a79217545
Ivan Kotelnikov <ivan.kotelnikov@bitfury.com>
2020-04-21 20:16:30 +0300

    export selected

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,9 @@
+import csv
+
 from django.contrib import admin
 from django import forms
 from dal import autocomplete
+from django.http import HttpResponse
 
 from .models import *
 
@@ -72,3 +75,19 @@ class TriggersAdmin(admin.ModelAdmin):
 @admin.register(AllowedChat)
 class ChatAdmin(admin.ModelAdmin):
     list_display = 'chat_id', 'title_chat', 'link_chat', 'status', 'creator'
+    actions = ["export_as_csv"]
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"

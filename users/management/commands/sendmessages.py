8a55868f672751d0c97bc3f9834c42f0b813859f
ivan kotelnikov <ivan.kotelnikov@bitfury.com>
2020-07-01 18:21:06 +0300

    preload file before send

--- users/management/commands/sendmessages.py
+++ users/management/commands/sendmessages.py
@@ -39,15 +39,17 @@ def broadcast_users(app, user_ids, text, doc=None):
     count_error = 0
     success_uids = []
     file_id, file_ref = None, None
+    if doc:
+        file = app.save_file(doc)
+        file_id = file.id
+        # print(file)
     for uid in user_ids:
         try:
             if doc:
                 # app.send_video(uid, doc, caption=text)
                 msg = app.send_document(uid, file_id or doc, file_ref=file_ref, caption=text)
-                if not file_id:
-                    file_id = str(msg.video.file_id)
-                if not file_ref:
-                    file_ref = msg.video.file_ref
+                # if not file_ref:
+                #     file_ref = msg.video.file_ref
             else:
                 app.send_message(uid, text)
             success_uids.append(uid)

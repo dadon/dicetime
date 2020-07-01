bd4b23d466b97b68f2d7b72211840e8ce35618ac
ivan kotelnikov <ivan.kotelnikov@bitfury.com>
2020-07-01 17:13:08 +0300

    update broadcast ads

--- users/management/commands/sendmessages.py
+++ users/management/commands/sendmessages.py
@@ -6,23 +6,21 @@ from pyrogram import Client
 from pyrogram.errors import FloodWait, RPCError, ChannelInvalid, ChannelPrivate, PeerIdInvalid
 
 from dice_time.settings import TG_API_ID, TG_API_HASH, API_TOKEN
+from users.models import User
 
 logger = logging.getLogger('Dice')
 
 
+FS_PROMO_VIDEO = 'content/cube.mp4'
 FS_PROMO_TEXT = '''
-Привет, у меня хорошие новости! 
+Привет! 
 
-Добавилась новая команда - **fs**
+Появилась новая крутая игрушка c Minter монетой `CUBE`
+Игра так и называется **-=Cube=-**
 
-Если ее написать в чате, в котором есть я, то появится специальная ссылка на секретную виртуальную комнату этого чата. В ней можно общаться с помощью видео связи прямо в браузере, без установки каких-либо дополнительных программ. 
+Регистрация через моего собутыльника - бота @CubeMinterBot
 
-Комната создана в Friendoscope, отсюда и название команды - **fs** (**F**riendo**S**cope)
-
-В этой комнате может общаться сразу несколько человек. Можно заходить и выходить в любой момент.  
-Можно играть в игры, рисовать приличные рисунки и шарить экран. Выпивать и распевать. Изливать и обучать. Скидывать смешные гифки и серьезные картинки. Смотреть вместе youtube ролики, наконец.  
-
-Ну и, конечно, просто болтать с друзьями
+PS: Они там всем новым игрокам дают по 10 `CUBE`!
 '''
 
 
@@ -35,13 +33,19 @@ def collect_chat_members(app: Client, chat_id, chat_title=None):
         return []
 
 
-def broadcast_users(app, user_ids, text):
+def broadcast_users(app, user_ids, text, doc=None):
     logger.info(f'Broadcast for {len(user_ids)} started.')
     count_success = 0
     count_error = 0
+    success_uids = []
     for uid in user_ids:
         try:
-            app.send_message(uid, text)
+            if doc:
+                print(doc, FS_PROMO_TEXT)
+                app.send_document(uid, doc, caption=text)
+            else:
+                app.send_message(uid, text)
+            success_uids.append(uid)
             count_success += 1
         except FloodWait as exc:
             sleep(exc.x)
@@ -52,6 +56,7 @@ def broadcast_users(app, user_ids, text):
             logger.info(f'\n\n{type(exc)}: {exc}\n\n')
             count_error += 1
     logger.info(f'Broadcast done. count_success={count_success} count_error={count_error}')
+    return success_uids
 
 
 def broadcast_chat_members(app, chat_ids, text):
@@ -66,20 +71,38 @@ def broadcast_chat_members(app, chat_ids, text):
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
-        parser.add_argument('--chatlist')
+        parser.add_argument('--fchatlist')
+        parser.add_argument('--uall', action='store_true')
+        parser.add_argument('--utest')
 
     def handle(self, **options):
-        chat_list = options.get('chatlist')
-        chat_ids = []
-        if not chat_list:
-            return
-        with open(chat_list, 'r') as f:
-            for line in f:
-                chat_ids.append(int(line))
-
-        if not chat_ids:
-            logger.info('Chatlist empty :(')
-        logger.info(f'Got {len(chat_ids)} chats')
-
         with Client('pyrosession', api_id=TG_API_ID, api_hash=TG_API_HASH, bot_token=API_TOKEN) as app:
-            broadcast_chat_members(app, chat_ids, FS_PROMO_TEXT)
+            chat_list_filename = options.get('fchatlist')
+            uall = options.get('uall')
+            utest = options.get('utest')
+
+            chat_ids = []
+            user_ids = []
+
+            if chat_list_filename:
+                with open(chat_list_filename, 'r') as f:
+                    for line in f:
+                        chat_ids.append(int(line))
+
+            if chat_ids:
+                logger.info(f'Got {len(chat_ids)} chats')
+                broadcast_chat_members(app, chat_ids, FS_PROMO_TEXT)
+                return
+
+            if uall:
+                user_ids = [u.id for u in User.objects.all()]
+            elif utest:
+                user_ids = [utest]
+
+            if user_ids:
+                logger.info(f'Got {len(user_ids)} users')
+                success_ids = broadcast_users(app, user_ids, FS_PROMO_TEXT, doc=FS_PROMO_VIDEO)
+                logger.info('########## success ids ##########')
+                logger.info('\n' + '\n'.join(success_ids))
+                logger.info('########## success ids ##########')
+                return
